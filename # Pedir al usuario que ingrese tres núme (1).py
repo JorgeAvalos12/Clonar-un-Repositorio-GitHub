59d92595ad50ba86466d14b75410cd9ec7e12e98
JorgeAvalos12 <j.avalos@institutoelcondor.edu.ec>
2025-03-09 11:03:47 -0500

    Add files via upload

---
+++ # Pedir al usuario que ingrese tres núme (1).py
@@ -0,0 +1,68 @@
+import ssl
+
+
+numero1 = 5
+numero2 = 7
+numero3 = 3 
+suma = numero1+numero2+numero3
+#print("el resultado de la suma es :" + str(suma))
+print(f"el resultado es{suma}")
+
+
+
+#Une tres palabras en una sola oración 
+palabra1 = "el" 
+
+palabra2 = "sol"
+
+palabra3 = "quema"
+
+suma = palabra1 + palabra2 + palabra3
+
+#print("el resultado de la suma es :" + int(suma))
+print(f"el resultado es{suma}")
+
+
+
+#Calcular el area de un cuadrado
+#definir la variable del lado
+
+lado = 6
+
+#calcular el area del cuadrado
+area=lado**2
+
+print("El área del cuadrado es:", area)
+
+
+
+#Calcular el perimetro de un rectangulo
+
+largo = 10
+ancho = 6
+
+#calcular el perimetro del rectangulo
+
+perimetro = 2 * (largo + ancho)
+print("el perimetro del rectangulo es :", perimetro)
+
+
+
+
+#Convierte una temperatura en grados Celsius a Fahrenheit 
+
+celsius = 18
+# Convertir la temperatura de Celsius a Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+print("La temperatura en Fahrenheit es:", fahrenheit)
+
+
+
+
+#calcular el area de un cuerpo geometrico que posee 11 lados
+
+hen = 11
+la= 4
+area = (11 / 4) * la**2 * (1 /math.tan( math.pi / 11))
+# Calcular el área utilizando la fórmula
+print("El área del polígono de", hen, "lados es:", area)
